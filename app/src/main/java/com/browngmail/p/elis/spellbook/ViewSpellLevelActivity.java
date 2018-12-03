3616df20e89de684fbf9bc192bec3c35b8e27f43
elispb <elis.p.brown@gmail.com>
2018-12-03 22:49:50 +0000

    Working on the add spell screen
    Layout needs some more thought

--- app/src/main/java/com/browngmail/p/elis/spellbook/ViewSpellLevelActivity.java
+++ app/src/main/java/com/browngmail/p/elis/spellbook/ViewSpellLevelActivity.java
@@ -3,6 +3,8 @@ package com.browngmail.p.elis.spellbook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ViewSpellLevelActivity extends AppCompatActivity {
@@ -14,5 +16,13 @@ public class ViewSpellLevelActivity extends AppCompatActivity {
         TextView title = findViewById(R.id.SpellLevelTitle);
         Intent intent = getIntent();
         title.setText(intent.getStringExtra("level"));
+
+        Button AddSpell = findViewById(R.id.AddSpellButton);
+        AddSpell.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AddSpellActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
